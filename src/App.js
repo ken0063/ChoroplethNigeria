7fdbb795bafb0ae13287efca0532cc73956e1439
ken0063 <olukotunk@gmail.com>
2023-02-18 08:46:59 +0100

    Nivo Map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { ResponsiveChoropleth } from "@nivo/geo";
+import { data } from "./data";
+import { match } from "./seacrhData";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          width: "90%",
+          height: "90%",
+        }}
+      >
+        <ResponsiveChoropleth
+          data={match}
+          features={data?.features}
+          margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+          colors="nivo"
+          unknownColor="#ffffff"
+          domain={[0, 1000000]}
+          label="properties.id"
+          valueFormat=".2s"
+          projectionScale={5000}
+          projectionTranslation={[0.15, 1.3]}
+          projectionRotation={[0, 0, 0]}
+          enableGraticule={false}
+          borderWidth={0.5}
+          isInteractive={true}
+          borderColor="#111111"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/seacrhData.js
@@ -0,0 +1,150 @@
+export const match = [
+  {
+    id: "ABI",
+    value: 336354,
+  },
+  {
+    id: "ADA",
+    value: 296583,
+  },
+  {
+    id: "AKI",
+    value: 364571,
+  },
+  {
+    id: "ANA",
+    value: 610382,
+  },
+  {
+    id: "BAU",
+    value: 794809,
+  },
+  {
+    id: "BAY",
+    value: 923418,
+  },
+  {
+    id: "BEN",
+    value: 342555,
+  },
+  {
+    id: "BOR",
+    value: 598836,
+  },
+  {
+    id: "COR",
+    value: 718056,
+  },
+  {
+    id: "DEL",
+    value: 208817,
+  },
+  {
+    id: "EBO",
+    value: 360414,
+  },
+  {
+    id: "EDO",
+    value: 876948,
+  },
+  {
+    id: "EKI",
+    value: 655100,
+  },
+  {
+    id: "ENU",
+    value: 232606,
+  },
+  {
+    //     id: "FCT",
+    //     value: 417156,
+  },
+  {
+    id: "GOM",
+    value: 854162,
+  },
+  {
+    id: "IMO",
+    value: 388528,
+  },
+  {
+    id: "JIG",
+    value: 111098,
+  },
+  {
+    id: "KAD",
+    value: 740178,
+  },
+  {
+    id: "KAN",
+    value: 667428,
+  },
+  {
+    id: "KAT",
+    value: 859564,
+  },
+  {
+    id: "KEB",
+    value: 455763,
+  },
+  {
+    id: "KOG",
+    value: 286525,
+  },
+  {
+    id: "KWA",
+    value: 541041,
+  },
+  {
+    id: "LAG",
+    value: 231176,
+  },
+  {
+    id: "NAS",
+    value: 526976,
+  },
+  {
+    id: "NIG",
+    value: 551696,
+  },
+  {
+    id: "OGU",
+    value: 883340,
+  },
+  {
+    id: "OND",
+    value: 260808,
+  },
+  {
+    id: "OSU",
+    value: 502330,
+  },
+  {
+    id: "OYO",
+    value: 952636,
+  },
+  {
+    id: "PLA",
+    value: 446989,
+  },
+  {
+    id: "RIV",
+    value: 170799,
+  },
+  {
+    id: "SOK",
+    value: 58530,
+  },
+  {
+    id: "TAR",
+    value: 385820,
+  },
+  {
+    id: "YOB",
+    value: 639105,
+  },
+  {
+    id: "ZAM",
+    value: 11567,
+  },
+];
